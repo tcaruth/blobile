f145be71d08aa8e588217c03f0394b5b3547088c
CircleCI Automated Commit <github@raccoonfink.com>
2020-11-16 21:10:00 -0500

    build: automated merge from v2.x

---
+++ docs/status-tap-b46a1b02-js-es2015.4a87af5efa1f158b0199.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{s0NH:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",function(){return s});var o=e("A36C");const s=()=>{const n=window;n.addEventListener("statusTap",()=>{Object(o.h)(()=>{const t=document.elementFromPoint(n.innerWidth/2,n.innerHeight/2);if(!t)return;const e=t.closest("ion-content");e&&e.componentOnReady().then(()=>{Object(o.f)(()=>e.scrollToTop(300))})})})}}}]);
+//# sourceMappingURL=status-tap-b46a1b02-js-es2015.4a87af5efa1f158b0199.js.map
